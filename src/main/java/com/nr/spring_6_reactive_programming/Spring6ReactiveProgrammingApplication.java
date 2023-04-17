4843ad624673f6baa7650ead31bb42de68bc6dc2
NitinNitinRaj <bnitinraj@gmail.com>
2023-04-17 10:38:43 +0530

    Initial commit

---
+++ src/main/java/com/nr/spring_6_reactive_programming/Spring6ReactiveProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.nr.spring_6_reactive_programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6ReactiveProgrammingApplication.class, args);
+	}
+
+}
